e664289d52742fe2d17709c3c07c1b4a2d851bd8
Lutfi Azhar <m.lutfiazhar@gmail.com>
2022-04-17 21:23:52 +0700

    added xcframework

---
+++ KonnekSDK.xcframework/ios-arm64/KonnekSDK.framework/Headers/KonnekSDKIOS.h
@@ -0,0 +1,18 @@
+//
+//  KonnekSDKIOS.h
+//  KonnekSDKIOS
+//
+//  Created by Lutfi Azhar on 12/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KonnekSDK.
+FOUNDATION_EXPORT double KonnekSDKVersionNumber;
+
+//! Project version string for KonnekSDK.
+FOUNDATION_EXPORT const unsigned char KonnekSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KonnekSDK/PublicHeader.h>
+
+
